fd6c081b883551cf44130da8244955e5af0ac312
saloniprajapat2404 <saloniprajapat977@gmail.com>
2026-09-18 14:07:12 +0530

    Connect frontend to live backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://waste-worth.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
